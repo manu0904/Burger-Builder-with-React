0149ffc28e713f3c133162139da360fe97569dea
manu0904 <muditkumar.0910@gmail.com>
2020-01-04 18:17:39 +0530

    Burger-Builder-React-App

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Styles from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+
+const sideDrawer = ( props ) => {
+    let attachedClasses = [Styles.SideDrawer, Styles.Close];
+    if (props.open) {
+        attachedClasses = [Styles.SideDrawer, Styles.Open];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={Styles.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+};
+
+export default sideDrawer;
